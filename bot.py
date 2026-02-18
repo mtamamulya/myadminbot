a541d0407c80ac903601a23da5fece49f104d18c
mtamamulya <mtamamulya@gmail.com>
2026-02-18 10:39:24 +0700

    komentar komit

--- bot.py
+++ bot.py
@@ -12,6 +12,12 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
 
+@bot.event
+async def on_member_join(member):
+    # Mengirim pesan ucapan selamat
+    for channel in member.guild.text_channels:
+        await channel.send(f'Selamat datang, {member.mention}!')
+
 @bot.command()
 async def start(ctx):
     await ctx.send("Hi! I'm a chat manager bot!")
